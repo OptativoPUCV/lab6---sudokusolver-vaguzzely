b3c9c471e4b058fe1423ea67bcf40e56994082d1
Replit user <>
2023-05-07 03:24:45 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -43,68 +43,44 @@ void print_node(Node* n){
     printf("\n");
 }
 
-int repetidosFila(int fila, Node *n)
-{
-  int numFila[10] = {0}, columna;
-
-  for(columna = 0 ; columna < 9 ; columna++)
-  {
-    if(n -> sudo[fila][columna] != 0)
-    {
-      if (numFila[n -> sudo[fila][columna]] == 1) return 0;
-      else numFila[n -> sudo[fila][columna]] = 1;
-    }
-  }
-  
-  return 1;
-}
-
-int repetidosColumna(int columna, Node *n)
-{
-  int numColumna[10] = {0}, fila;
-
-  for(fila = 0 ; fila < 9 ; fila++)
-  {
-    if(n -> sudo[fila][columna] != 0)
-    {
-      if (numColumna[n -> sudo[fila][columna]] == 1) return 0;
-      else numColumna[n -> sudo[fila][columna]] = 1;
-    }
-  }
-  
-  return 1;
-}
-
-int repetidosSubMatriz(int subMatriz, Node *n)
-{
-  int aux[10] = {0}, p;
-  
-  for(p = 0 ; p < 9 ; p++)
-  {
-    int i = 3 * (subMatriz/3) + (p/3);
-    int j = 3 * (subMatriz%3) + (p%3);
+int is_valid(Node * n) {
+    int rows[9][10] = {0}; // arreglo de enteros para marcar los números en cada fila
+    int cols[9][10] = {0}; // arreglo de enteros para marcar los números en cada columna
+    int boxes[9][10] = {0}; // arreglo de enteros para marcar los números en cada submatriz 3x3
     
-    if(n -> sudo[i][j] != 0)
-    {
-      if (aux[n -> sudo[i][j]] == 1) return 0;
-      else aux[n -> sudo[i][j]] = 1;
+    // recorrer las celdas del estado/nodo
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            int num = n->board[i][j];
+            if (num != 0) { // si la celda no está vacía
+                // verificar si el número ya apareció en la fila
+                if (rows[i][num] == 1) {
+                    return 0; // no es válido
+                } else {
+                    rows[i][num] = 1; // marcar el número en la fila
+                }
+                
+                // verificar si el número ya apareció en la columna
+                if (cols[j][num] == 1) {
+                    return 0; // no es válido
+                } else {
+                    cols[j][num] = 1; // marcar el número en la columna
+                }
+                
+                // verificar si el número ya apareció en la submatriz 3x3
+                int box_idx = (i / 3) * 3 + (j / 3); // índice de la submatriz
+                if (boxes[box_idx][num] == 1) {
+                    return 0; // no es válido
+                } else {
+                    boxes[box_idx][num] = 1; // marcar el número en la submatriz
+                }
+            }
+        }
     }
-  }
-  
-  return 1;
+    
+    return 1; // es válido
 }
 
-int is_valid(Node* n)
-{
-  int i;
-  for (i = 0 ; i < 9 ; i++)
-  {
-    if(repetidosFila(i, n) == 0 || repetidosColumna(i, n) == 0 || repetidosSubMatriz(i, n) == 0) return 0;
-  }
-  
-  return 1;
-  
-}
 
 
 
