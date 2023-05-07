7eaf980cffbf58cc2a700143bdb788a888511613
Replit user <>
2023-05-07 03:07:59 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -43,65 +43,8 @@ void print_node(Node* n){
     printf("\n");
 }
 
-int verificarFilas(int fila, Node* nodo) {
-  int num[10] = {0};
-  int columna;
-
-  for (columna = 0; columna < 9; columna++) {
-    int valor = nodo->sudo[fila][columna];
-    if (valor != 0) {
-      if (num[valor] == 1) {
-        return 0;
-      } else {
-        num[valor] = 1;
-      }
-    }
-  }
-  return 1;
-}
-
-
-int repetidosColumna(int columna, Node *n)
-{
-  int numColumna[10] = {0}, fila;
-
-  for(fila = 0 ; fila < 9 ; fila++)
-  {
-    if(n -> sudo[fila][columna] != 0)
-    {
-      if (numColumna[n -> sudo[fila][columna]] == 1) return 0;
-      else numColumna[n -> sudo[fila][columna]] = 1;
-    }
-  }
-  
-  return 1;
-}
-
-int repetidosSubMatriz(int subMatriz, Node *n)
-{
-  int aux[10] = {0}, p;
-  
-  for(p = 0 ; p < 9 ; p++)
-  {
-    int i = 3 * (subMatriz/3) + (p/3);
-    int j = 3 * (subMatriz%3) + (p%3);
-    
-    if(n -> sudo[i][j] != 0)
-    {
-      if (aux[n -> sudo[i][j]] == 1) return 0;
-      else aux[n -> sudo[i][j]] = 1;
-    }
-  }
-  
-  return 1;
-}
 int is_valid(Node* n)
 {
-  int i;
-  for (i = 0 ; i < 9 ; i++)
-  {
-    if(verificarFilas(i, n) == 0 || repetidosColumna(i, n) == 0 || repetidosSubMatriz(i, n) == 0) return 0;
-  }
   
 }
 
@@ -150,7 +93,24 @@ int is_final(Node* n)
 
 Node* DFS(Node* initial, int* cont)
 {
-  
+  Stack* s = createStack();
+  push(s,initial);
+  while(!is_empty(s)){
+    (*cont)++;
+    Node *n = top(s);
+    pop(s);
+    if(is_final(n)){
+      print_node(n);
+      return n;
+    }
+    List* adj=get_adj_nodes(n);
+    Node* aux= first(adj);
+    while(aux){
+      push(s,aux);
+      aux=next(adj);
+    }
+    free(n);
+  }
   return NULL;
 }
 
