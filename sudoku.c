01c243353a48a5350d211102db555daacf185934
Replit user <>
2023-05-07 03:20:08 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -44,60 +44,59 @@ void print_node(Node* n){
 }
 
 int is_valid(Node* n){
-  int i,j,k,p;
+  int i,j,k,l,p;
   int aux;
   int *arreglo = (int*) calloc(10,sizeof(int));
+  
+  // Validación de filas
   for(j = 0 ; j < 9 ; j++){
     for(i = 0; i < 9; i++){
       if(n->sudo[i][j] != 0){
         aux = n->sudo[i][j];
         arreglo[aux]++;
-        if (arreglo[aux] > 1)return 0;
+        if (arreglo[aux] > 1) return 0;
       }
     }
-    for(k = 0; k < 10;k++){
+    for(k = 0; k < 10; k++){
       arreglo[k] = 0;
     }
   }
   
-  for(k = 0; k < 10;k++){
-    arreglo[k] = 0;
-  }
-  
+  // Validación de columnas
   for(i = 0 ; i < 9 ; i++){
     for(j = 0; j < 9; j++){
       if(n->sudo[i][j] != 0){
         aux = n->sudo[i][j];
         arreglo[aux]++;
-        if (arreglo[aux] > 1)return 0;
+        if (arreglo[aux] > 1) return 0;
       }
     }
-    for(k = 0; k < 10;k++){
+    for(k = 0; k < 10; k++){
       arreglo[k] = 0;
     }
   }
 
-  for(k = 0; k < 10;k++){
-      arreglo[k] = 0;
-    }
-  
-  for(k = 0; k < 9 ; k++){ 
-    for(p=0;p<9;p++){
-      i=3*(k/3) + (p/3) ;
-      j=3*(k%3) + (p%3) ;
-      if(n->sudo[i][j] != 0){
-        aux = n->sudo[i][j];
-        arreglo[aux]++;
-        if(arreglo[aux] > 1)return 0; 
+  // Validación de submatrices
+  for(k = 0; k < 3; k++){ 
+    for(l = 0; l < 3; l++){
+      for(p = 0; p < 9; p++){
+        i = 3*k + (p/3);
+        j = 3*l + (p%3);
+        if(n->sudo[i][j] != 0){
+          aux = n->sudo[i][j];
+          arreglo[aux]++;
+          if (arreglo[aux] > 1) return 0; 
+        }
+      }
+      for(k = 0; k < 10; k++){
+        arreglo[k] = 0;
       }
     }
-    for(k = 0; k < 10;k++){
-      arreglo[k] = 0;
-    }
   }
-  //print_node(n);
+  
   return 1;
 }
+
 
 
 List* get_adj_nodes(Node* n)
